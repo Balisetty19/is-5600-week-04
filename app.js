9d76b01a7bdd6d2f5cf006a7a94220858ada4f86
Balisetty19 <nagadinesh.balisetty@slu.edu>
2024-11-15 01:08:25 +0000

    lab_04

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
+const api = require('./api')
+const middleware = require('middleware')
+// app.js
+// Add body parser middleware
+const bodyParser = require('body-parser')
+
 
 // Set the port
 const port = process.env.PORT || 3000
 // Boot the app
 const app = express()
 // Register the public directory
+app.use(middleware.corse)
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 // register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+app.get('/products', api.listProducts)
+app.get('/', api.handleRoot);
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
-/**
- * Handle the root route
- * @param {object} req
- * @param {object} res
-*/
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-}
-
-/**
- * List all products
- * @param {object} req
- * @param {object} res
- */
-async function listProducts(req, res) {
-  const productsFile = path.join(__dirname, 'data/full-products.json')
-  try {
-    const data = await fs.readFile(productsFile)
-    res.json(JSON.parse(data))
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-}
